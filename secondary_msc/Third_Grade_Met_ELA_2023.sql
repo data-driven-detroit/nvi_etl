e9674461f4704c2ad06dbb4d5bc5c3508bbde3c1
abhiD3 <abhi@datadrivendetroit.org>
2025-03-11 12:41:20 -0400

    Everything Works

--- secondary_msc/Third_Grade_Met_ELA_2023.sql
+++ secondary_msc/Third_Grade_Met_ELA_2023.sql
@@ -16,10 +16,10 @@ WHERE year = '2023';
 with school as (SELECT district_code, district_name, building_code, building_name, council_di
 FROM education.eem_schools as sch
 JOIN shp."Detroit_City_Council_Districts_2026" cb
-ON st_intersects(cb.geom,sch.geometry)
+ON st_intersects(st_transform(cb.geom,4326),sch.geometry)
 WHERE sch.start_date >= '2023-07-01' AND sch.end_date <= '2024-06-29')
 
-SELECT s.council_di, sum(number_assessed) as number_assessed_2023, sum(total_met) as met_ela_2023, (sum(total_met) / sum(number_assessed)) * 100  AS percentage_met_ela_2023
+SELECT s.council_di as council_districts, sum(number_assessed) as number_assessed_2023, sum(total_met) as met_ela_2023, (sum(total_met) / sum(number_assessed)) * 100  AS percentage_met_ela_2023
 FROM school as s
 join education.g3_ela_school as e
 on s.building_code = e.building_code
@@ -27,15 +27,15 @@ WHERE year = '2023'
 GROUP BY s.council_di;
 
 --- NVI Zones
-with school as (SELECT district_code, district_name, building_code, building_name, cb."DistrictNu", cb."ZoneName"
+with school as (SELECT district_code, district_name, building_code, building_name, cb.district_n, cb.zone_id
 FROM education.eem_schools as sch
-JOIN shp."Temp_NVIZones2026v2__20250226" cb
-ON st_intersects(sch.geometry, cb.geom)
+JOIN shp.nvi_neighborhood_zones_temp_2025 cb
+ON st_intersects(sch.geometry, st_transform(cb.geom,4326))
 WHERE sch.start_date >= '2023-07-01' AND sch.end_date <= '2024-06-29')
 
-SELECT s."DistrictNu", s."ZoneName", sum(number_assessed) as number_assessed_2023, sum(total_met) as met_ela_2023, (sum(total_met) / sum(number_assessed)) * 100  AS percentage_met_ela_2023
+SELECT s.district_n as district_number, s.zone_id as zone_id, sum(number_assessed) as number_assessed_2023, sum(total_met) as met_ela_2023, (sum(total_met) / sum(number_assessed)) * 100  AS percentage_met_ela_2023
 FROM school as s
 join education.g3_ela_school as e
 on s.building_code = e.building_code
 WHERE year = '2023'
-GROUP BY s."DistrictNu", s."ZoneName";
+GROUP BY s.district_n, s.zone_id;
