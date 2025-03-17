2c0c6037041980fd2aa48fc46537a11c1fe2448e
mikevatd3 <mike@datadrivendetroit.org>
2025-03-17 08:18:55 -0400

    made updates to primary survey

--- primary_survey/2024/archive_data.py
+++
@@ -1,5 +0,0 @@
-def archive_data():
-    print("Archiving data...")
-    #TODO: move the data from the extract location to the archive location for processed data (if appropriate)
-    #TODO: consider automated alerts here - email the team, add an asana comment, etc. to notify that the data has been processed and available for QA
-    print("Data archived successfully!")
