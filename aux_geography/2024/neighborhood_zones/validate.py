21d45a70051ae40b3f1275d291812b8cb9679ad2
Mike V <mike@datadrivendetroit.org>
2025-03-25 16:30:49 -0400

    deleted empty ipynb file

--- aux_geography/2024/neighborhood_zones/validate.py
+++ aux_geography/2024/neighborhood_zones/validate.py
@@ -3,7 +3,10 @@ import pandera as pa
 from pandera.typing.geopandas import GeoSeries
 
 
-class DetroitCityBoundary(pa.DataFrameModel):
+class NVINeighborhoodZones(pa.DataFrameModel):
+    zone_id: str = pa.Field()
+    district_number: int = pa.Field()
+    square_miles: float = pa.Field()
     start_date: date = pa.Field()
     end_date: date = pa.Field()
     geometry: GeoSeries = pa.Field()
