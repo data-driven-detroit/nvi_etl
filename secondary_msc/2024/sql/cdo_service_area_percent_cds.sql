379828942624c394212213a235c289a66309e159
Mike V <mike@datadrivendetroit.org>
2025-03-27 09:05:26 -0400

    fixed zone coverage queries

--- secondary_msc/2024/sql/cdo_service_area_percent_cds.sql
+++ secondary_msc/2024/sql/cdo_service_area_percent_cds.sql
@@ -3,20 +3,21 @@ WITH coverage AS (
     SELECT st_union(geom) AS geom
     FROM shp.becdd_47cdoserviceareas_20220815
 ),
-WITH counts AS (
+counts AS (
     SELECT district_number, count(*) AS num_cdos
     FROM nvi.detroit_council_districts cd
     JOIN shp.becdd_47cdoserviceareas_20220815 orgs
-        ON ST_INTERSECTS(ST_TRANSFORM(cd.geom, orgs.geom)
-    GROUP BY district_number;
+        ON ST_INTERSECTS(ST_TRANSFORM(cd.geometry, 4326), orgs.geom)
+    GROUP BY district_number
 )
 SELECT
     'council_districts' AS geo_type,
     cd.district_number AS geography,
     counts.num_cdos,
-    ST_AREA(ST_INTERSECTION(ST_TRANSFORM(cd.geom, 4326), cov.geom)) * 100
-    / ST_AREA(ST_TRANSFORM(cd.geom, 4326)) AS pct_cdo_coverage
+    ST_AREA(ST_INTERSECTION(ST_TRANSFORM(cd.geometry, 4326), cov.geom)) * 100
+    / ST_AREA(ST_TRANSFORM(cd.geometry, 4326)) AS pct_cdo_coverage
 FROM nvi.detroit_council_districts cd
     JOIN coverage cov ON TRUE  -- JOIN the combined shape to every for calc
 JOIN counts 
     ON counts.district_number = cd.district_number;
+
