bef5e906abe169f3f688a4e29b5303428fa549df
Mike V <mike@datadrivendetroit.org>
2025-05-29 15:00:35 -0400

    working on evictions

--- secondary_evictions/transform.py
+++ secondary_evictions/transform.py
@@ -8,8 +8,14 @@ WORKING_DIR = Path(__file__).resolve().parent
 
 def transform(logger):
     frame = pd.read_csv(WORKING_DIR / "input" / "evictions_anon_point_level.csv")
+    frame["year"] = pd.to_datetime(frame["filed_date"]).dt.year
 
-    print(frame.head())
+    pd.concat(
+        [
+            frame.groupby(["zone_id", "year"], dropna=False).size().rename(columns={"zone_id": "geography"}),
+            frame.groupby(["district_number", "year"], dropna=False).size().rename(columns={"district_number": "geography"}),
+            frame.groupby(["year"], dropna=False).size(),
+        ]
 
 
 if __name__ == "__main__":
